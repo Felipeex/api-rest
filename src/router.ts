94d712ea4363665fa151a762fe051760daf780f2
Felipeex <F3ZINH0HG@gmail.com>
2022-09-11 03:43:38 -0300

    test github action

--- src/router.ts
+++ src/router.ts
@@ -16,7 +16,7 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 
   return res.status(500).json({
     status: "error",
-    message: `Internal server error - ${err}`,
+    message: `Internal server error - ${err} test`,
   });
 });
 
